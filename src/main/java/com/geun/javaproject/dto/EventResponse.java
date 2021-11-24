964fa768f7551536c02292aed6f7247e591d82c0
Geun Woo <lkwoo98131@naver.com>
2021-11-25 01:37:57 +0900

    Add #1 - DTO
    테스트를 진행할 event와 place 먼저 구현

---
+++ src/main/java/com/geun/javaproject/dto/EventResponse.java
@@ -0,0 +1,56 @@
+package com.geun.javaproject.dto;
+
+import com.geun.javaproject.constant.EventStatus;
+
+import java.time.LocalDateTime;
+
+public record EventResponse(
+        Long id,
+        Long placeId,
+        String eventName,
+        EventStatus eventStatus,
+        LocalDateTime eventStartDatetime,
+        LocalDateTime eventEndDatetime,
+        Integer currentNumberOfPeople,
+        Integer capacity,
+        String memo
+) {
+    public static EventResponse of(
+            Long id,
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Integer currentNumberOfPeople,
+            Integer capacity,
+            String memo
+    ) {
+        return new EventResponse(
+                id,
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
+        );
+    }
+
+    public static EventResponse from(EventDTO eventDTO) {
+        if (eventDTO == null) { return null; }
+        return EventResponse.of(
+                eventDTO.id(),
+                eventDTO.placeId(),
+                eventDTO.eventName(),
+                eventDTO.eventStatus(),
+                eventDTO.eventStartDatetime(),
+                eventDTO.eventEndDatetime(),
+                eventDTO.currentNumberOfPeople(),
+                eventDTO.capacity(),
+                eventDTO.memo()
+        );
+    }
+}
