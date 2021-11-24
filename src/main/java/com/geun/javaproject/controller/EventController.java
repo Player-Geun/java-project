be417e36ae8b0407dfa8d4ed30cf5ad0556dc214
Geun Woo <lkwoo98131@naver.com>
2021-11-25 01:57:04 +0900

    Implement #1 - EventController
    비지니스 로직을 추가하고 ModelAndView 를 리턴하는 것으로 기능 향상

--- src/main/java/com/geun/javaproject/controller/EventController.java
+++ src/main/java/com/geun/javaproject/controller/EventController.java
@@ -1,22 +1,50 @@
 package com.geun.javaproject.controller;
 
+import com.geun.javaproject.domain.Event;
+import com.geun.javaproject.dto.EventResponse;
+import com.geun.javaproject.service.EventService;
+import com.querydsl.core.types.Predicate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
 @RequestMapping("/events")
 @Controller
 public class EventController {
 
+    private final EventService eventService;
+
     @GetMapping
-    public String events() {
-        return "event/index";
+    public ModelAndView events(@QuerydslPredicate(root = Event.class)Predicate predicate) {
+        Map<String, Object> map = new HashMap<>();
+        List<EventResponse> events = eventService.getEvents(predicate)
+                .stream()
+                .map(EventResponse::from)
+                .toList();
+        map.put("events", events);
+
+        return new ModelAndView("event/index", map);
     }
 
     @GetMapping("/{eventId}")
-    public String eventDetail(@PathVariable Long eventId) {
-        return "event/detail";
+    public ModelAndView eventDetail(@PathVariable Long eventId) {
+        Map<String, Object> map = new HashMap<>();
+        EventResponse event = eventService.getEvent(eventId)
+                .map(EventResponse::from)
+                .orElseThrow();
+
+        map.put("event", event);
+
+        return new ModelAndView("event/detail", map);
     }
 
 }
