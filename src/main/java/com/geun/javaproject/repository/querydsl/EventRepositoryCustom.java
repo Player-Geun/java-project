19e29c959c481e98c9e9fa9669beda84fb08f508
Geun Woo <lkwoo98131@naver.com>
2021-12-02 01:19:44 +0900

    Add #2 - querydsl
    * EventRepositoryCustom을 EventRepository가 사용하게 함
    * EventRepositoryCustomImpl로 querydsl 구현체 작성
      * QuerydslRepositorySupport 지원을 사용
      * 다이나믹한 select 쿼리와 페이징 구현

---
+++ src/main/java/com/geun/javaproject/repository/querydsl/EventRepositoryCustom.java
@@ -0,0 +1,19 @@
+package com.geun.javaproject.repository.querydsl;
+
+import com.geun.javaproject.constant.EventStatus;
+import com.geun.javaproject.dto.EventViewResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+
+public interface EventRepositoryCustom {
+    Page<EventViewResponse> findEventViewPageBySearchParams(
+            String placeName,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Pageable pageable
+    );
+}
