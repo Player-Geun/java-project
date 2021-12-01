ff846bdbde3234698cf60e24f0b69673729b0368
Geun Woo <lkwoo98131@naver.com>
2021-12-02 01:26:02 +0900

    Implements #2 - controller
    기존의 controller를 수정된 다른 weblayer에 맞게 개선

--- src/main/java/com/geun/javaproject/controller/EventController.java
+++ src/main/java/com/geun/javaproject/controller/EventController.java
@@ -1,22 +1,33 @@
 package com.geun.javaproject.controller;
 
+import com.geun.javaproject.constant.ErrorCode;
+import com.geun.javaproject.constant.EventStatus;
 import com.geun.javaproject.domain.Event;
 import com.geun.javaproject.dto.EventResponse;
+import com.geun.javaproject.dto.EventViewResponse;
+import com.geun.javaproject.exception.GeneralException;
 import com.geun.javaproject.service.EventService;
 import com.querydsl.core.types.Predicate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/events")
 @Controller
 public class EventController {
@@ -24,12 +35,37 @@ public class EventController {
     private final EventService eventService;
 
     @GetMapping
-    public ModelAndView events(@QuerydslPredicate(root = Event.class)Predicate predicate) {
+    public ModelAndView events(@QuerydslPredicate(root = Event.class) Predicate predicate) {
         Map<String, Object> map = new HashMap<>();
         List<EventResponse> events = eventService.getEvents(predicate)
                 .stream()
                 .map(EventResponse::from)
                 .toList();
+
+        map.put("events", events);
+
+        return new ModelAndView("event/index", map);
+    }
+
+    @GetMapping("/custom")
+    public ModelAndView customEvents(
+            @Size(min = 2) String placeName,
+            @Size(min = 2) String eventName,
+            EventStatus eventStatus,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventStartDatetime,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventEndDatetime,
+            Pageable pageable
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        Page<EventViewResponse> events = eventService.getEventViewResponse(
+                placeName,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                pageable
+        );
+
         map.put("events", events);
 
         return new ModelAndView("event/index", map);
@@ -40,7 +76,7 @@ public class EventController {
         Map<String, Object> map = new HashMap<>();
         EventResponse event = eventService.getEvent(eventId)
                 .map(EventResponse::from)
-                .orElseThrow();
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
 
         map.put("event", event);
 
