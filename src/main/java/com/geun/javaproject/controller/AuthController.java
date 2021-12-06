003126cd9583d35fd49f18c87109e5b88b219a87
Geun Woo <lkwoo98131@naver.com>
2021-12-06 23:25:27 +0900

    Implements #3 - Controller
    Controller를 최종 업데이트한다.
    * 뷰와 url 연결
    * admin 구체화
    * auth 구체화

--- src/main/java/com/geun/javaproject/controller/AuthController.java
+++ src/main/java/com/geun/javaproject/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.geun.javaproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class AuthController {
@@ -11,9 +13,11 @@ public class AuthController {
         return "auth/login";
     }
 
-    @GetMapping("/sign-up")
-    public String signUp() {
-        return "auth/sign-up";
+    @GetMapping("/logout")
+    public String logout(@RequestHeader("referer") String referer, Model model) {
+        model.addAttribute("backUrl", referer);
+
+        return "auth/logout";
     }
 
 }
